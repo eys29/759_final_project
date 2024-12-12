b028a6d591bb7f97e717b3be0807b5a4b24f8730
Elise Song <eys29@cornell.edu>
2024-12-12 03:13:30 -0600

    timing

--- sift-cpp/cuda/cudasift.cu
+++ sift-cpp/cuda/cudasift.cu
@@ -16,12 +16,20 @@ int main(int argc, char *argv[])
         std::cerr << "Usage: ./match_features a.jpg b.jpg (or .png)\n";
         return 0;
     }
+    // keep on cpu
     Image a(argv[1]), b(argv[2]);
     a = a.channels == 1 ? a : rgb_to_grayscale(a);
     b = b.channels == 1 ? b : rgb_to_grayscale(b);
 
     // Start timing
-    auto start_time = std::chrono::high_resolution_clock::now();
+    // auto start_time = std::chrono::high_resolution_clock::now();
+    cudaEvent_t start;
+    cudaEvent_t stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    cudaEventRecord(start);
+
     std::vector<sift::Keypoint> kps_a = sift::find_keypoints_and_descriptors(a);
     std::vector<sift::Keypoint> kps_b = sift::find_keypoints_and_descriptors(b);
     //std::pair allows mix of heterogenous types in c++
@@ -31,8 +39,12 @@ int main(int argc, char *argv[])
     Image result = sift::draw_matches(a, b, kps_a, kps_b, matches);
     result.save("result.jpg");
     // End timing
-    auto end_time = std::chrono::high_resolution_clock::now();
-    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    //auto end_time = std::chrono::high_resolution_clock::now();
+    //auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    cudaEventRecord(stop);
+    cudaEventSynchronize(stop);
+    float elapsed;
+    cudaEventElapsedTime(&elapsed, start, stop);
     // Output results
     std::cout << "Found " << matches.size() << " feature matches. Output image is saved as result.jpg\n";
     std::cout << "Execution time: " << elapsed << " milliseconds.\n";
