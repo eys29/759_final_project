f033b6ec4a51ce3b0d7b6c03cd3b363cf11b89f9
Zhengxiong Li <zli2793@elab-vulcan.ece.wisc.edu>
2024-12-13 21:45:19 -0600

    Add timer for serial version

--- src/serial-sift/examples/match_features.cpp
+++ src/serial-sift/examples/match_features.cpp
@@ -4,6 +4,8 @@
 #include "image.hpp"
 #include "sift.hpp"
 
+#include <chrono>
+
 int main(int argc, char *argv[])
 {
     std::ios_base::sync_with_stdio(false);
@@ -17,12 +19,19 @@ int main(int argc, char *argv[])
     a = a.channels == 1 ? a : rgb_to_grayscale(a);
     b = b.channels == 1 ? b : rgb_to_grayscale(b);
 
+    // Start timing
+    auto start_time = std::chrono::high_resolution_clock::now();
     std::vector<sift::Keypoint> kps_a = sift::find_keypoints_and_descriptors(a);
     std::vector<sift::Keypoint> kps_b = sift::find_keypoints_and_descriptors(b);
     std::vector<std::pair<int, int>> matches = sift::find_keypoint_matches(kps_a, kps_b);
     Image result = sift::draw_matches(a, b, kps_a, kps_b, matches);
+    // End timing
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count();
+    // Now we can save the result
     result.save("result.jpg");
-    
+    // Output results
     std::cout << "Found " << matches.size() << " feature matches. Output image is saved as result.jpg\n";
+    std::cout << "Execution time: " << elapsed << " milliseconds.\n";
     return 0;
 }
