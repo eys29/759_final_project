7242d7f4a28fb60bede06c163ec6d5eaf8ee4402
Zhengxiong Li <zli2793@elab-vulcan.ece.wisc.edu>
2024-12-13 23:23:06 -0600

    temp folder

--- src/plot.py
+++ src/plot.py
@@ -14,14 +14,43 @@ def extract_number(filename):
     
     return result
 
+def helper_func():
+    # Opencv time
+    path = "./opencv-sift"
+    small_log = os.path.join(path, "small.txt")
+    medium_log = os.path.join(path, "medium.txt")
+    large_log = os.path.join(path, "large.txt")
+    
+    opencv_list = [small_log, medium_log, large_log]
+
+    # Openmp time
+    path = "./openmp-sift"
+    small_log = os.path.join(path, "small.txt")
+    medium_log = os.path.join(path, "medium.txt")
+    large_log = os.path.join(path, "large.txt")
+
+    openmp_list = [small_log, medium_log, large_log]
+    
+    # serial time
+    path = "./serial-sift"
+    small_log = os.path.join(path, "small.txt")
+    medium_log = os.path.join(path, "medium.txt")
+    large_log = os.path.join(path, "large.txt")
+    
+    serial_list = [small_log, medium_log, large_log]
+    
+    # cuda time
+    path = "./cuda-sift"
+    small_log = os.path.join(path, "small.txt")
+    medium_log = os.path.join(path, "medium.txt")
+    large_log = os.path.join(path, "large.txt")
+    
+    cuda_list = [small_log, medium_log, large_log]
+    
+    return serial_list, opencv_list, openmp_list, cuda_list
+    
+
 if __name__ == '__main__':
-    small_log = os.path.join("./opencv-sift", "small.txt")
-    medium_log = os.path.join("./opencv-sift", "medium.txt")
-    large_log = os.path.join("./opencv-sift", "large.txt")
+    serial_list, opencv_list, openmp_list, cuda_list = helper_func()
     
-    small_time = extract_number(small_log)
-    medium_time = extract_number(medium_log)
-    large_time = extract_number(large_log)
-    
-    print(small_time, medium_time, large_time)
     
